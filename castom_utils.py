ab4ebccb69eb37d9c3eddb2a981b2103d4891b47
aufflammen <aufflammen>
2024-12-19 16:03:27 +0300

    refactoring

--- castom_utils.py
+++ castom_utils.py
@@ -3,7 +3,8 @@ import json
 import time
 import numpy as np
 from pathlib import Path
-from typing import List, Optional, Any
+from collections.abc import Callable
+from typing import Any
 from PIL import Image
 
 from matplotlib import font_manager
@@ -23,43 +24,16 @@ class Ansi:
     underline = '\033[4m'
     end = '\033[0m'
 
-def write_json(path, obj) -> None:
+def write_json(path: str | Path, obj: Any) -> None:
+    """Запись объекта в *.json."""
     with open(path, 'w', encoding='utf-8') as file:
         json.dump(obj, file, indent=2, ensure_ascii=False)
 
-def read_json(path) -> Any:
+def read_json(path: str | Path) -> Any:
+    """Чтение *.json файла."""
     with open(path, 'r', encoding='utf-8') as file:
         obj = json.load(file)
     return obj
-
-#------------------
-# torch
-#------------------
-class DeNormalize(v2.Normalize):
-    """
-    Функция для денормализации изображений.
-    """
-    def __init__(self,mean, std, *args, **kwargs):
-        new_mean = [-m/s for m, s in zip(mean, std)]
-        new_std = [1/s for s in std]
-        super().__init__(new_mean, new_std, *args, **kwargs)
-
-
-def save_model(model, optimizer, scheduler, path, filename):
-    """
-    Сохранение весов моделей torch.
-    """
-    checkpoint = {
-        'info': {
-            'datetime': time.ctime(),
-        },
-        'model': model.state_dict(),
-        'optimizer': optimizer.state_dict(),
-        'scheduler': scheduler.state_dict()
-    }
-
-    path.mkdir(parents=True, exist_ok=True)
-    torch.save(checkpoint, path.joinpath(f'{filename}.pth'))
 
 
 #------------------
@@ -72,9 +46,7 @@ class AttrDict(dict):
 
 
 def load_config(path: str = 'config.yaml'):
-    """
-    Загрузка конфига.
-    """
+    """Загрузка конфига для проекта."""
     with open(path, 'r', encoding='utf-8') as file:
         config = yaml.load(file, Loader=yaml.loader.BaseLoader)
 
@@ -102,12 +74,42 @@ def load_config(path: str = 'config.yaml'):
 
 
 #------------------
+# torch
+#------------------
+class DeNormalize(v2.Normalize):
+    """Денормализации изображений."""
+    def __init__(self,mean, std, *args, **kwargs):
+        new_mean = [-m/s for m, s in zip(mean, std)]
+        new_std = [1/s for s in std]
+        super().__init__(new_mean, new_std, *args, **kwargs)
+
+
+def save_model(
+    model: torch.nn.Module, 
+    optimizer, 
+    scheduler, 
+    path: Path, 
+    filename: str
+) -> None:
+    """Сохранение весов моделей torch в файл."""
+    checkpoint = {
+        'info': {
+            'datetime': time.ctime(),
+        },
+        'model': model.state_dict(),
+        'optimizer': optimizer.state_dict(),
+        'scheduler': scheduler.state_dict()
+    }
+
+    path.mkdir(parents=True, exist_ok=True)
+    torch.save(checkpoint, path.joinpath(f'{filename}.pth'))
+
+
+#------------------
 # fonts / text
 #------------------
-def _is_supported_chars(font_path, vocab_unicode: List[int]):
-    """
-    Проверяет, поддерживает ли шрифт все нужные символы
-    """
+def _is_supported_chars(font_path: str, vocab_unicode: list[int]) -> bool:
+    """Проверяет, поддерживает ли шрифт все символы из vocab_unicode."""
     try:
         font = TTFont(font_path)
         cmap = font['cmap'].getBestCmap()
@@ -119,35 +121,30 @@ def _is_supported_chars(font_path, vocab_unicode: List[int]):
         return False
 
 
-def valid_fonts(fonts_list: List[str], vocab_unicode: List[int]):
-    """
-    Отбирает из списка шрифты, поддеривающие все необходимые символы
-    """
+def valid_fonts(fonts_list: list[str], vocab_unicode: list[int]) -> list[str]:
+    """Отбирает из списка шрифты, поддерживающие все символы из vocab_unicode."""
     return [font for font in fonts_list if _is_supported_chars(font, vocab_unicode)]
 
 
-def get_system_fonts():
-    """
-    Возвращает список системных шрифтов
-    """
+def get_system_fonts() -> list[str]:
+    """Возвращает список системных шрифтов."""
     # Получаем список всех шрифтов, установленных в системе
     fonts = font_manager.findSystemFonts(fontpaths=None, fontext='ttf')  # Системные шрифты
     return fonts
 
 
-def get_fonts_from_folder(path: str):
-    """
-    Возвращает список шрифтов из папки
-    """
+def get_fonts_from_folder(path: str) -> list[str]:
+    """Возвращает список шрифтов из папки."""
     path = Path(path)
     fonts = path.rglob('*.ttf')
     return [str(font) for font in fonts]
 
+def translate_text(text: str, translate: dict[str, str]) -> str:
+    """ Замена всех символов в тексте, согласно словарю translate.
 
-def translate_text(text: str, translate: dict[str, str]):
-    """
-    В translate передается словарь {"искомый символ": "заменить на"}
-    Все искомые символы заменяются на соответвующие, согласно словарю
+    Args:
+        text: Исходный текст.
+        translate: Словарь вида {"искомый символ": "заменить на"}.
     """
     translation_table = str.maketrans(translate)
     return text.translate(translation_table)
@@ -158,7 +155,7 @@ def translate_text(text: str, translate: dict[str, str]):
 #-----------------
 class RecognitionDatasetCustom(Dataset):
     
-    def __init__(self, path, transforms=None, transforms_alb=None):
+    def __init__(self, path: Path, transforms=None, transforms_alb=None):
         self.path = path
         self.transforms = transforms
         self.transforms_alb = transforms_alb
@@ -172,7 +169,7 @@ class RecognitionDatasetCustom(Dataset):
     def __len__(self):
         return len(self.img_names)
 
-    def __getitem__(self, idx):
+    def __getitem__(self, idx: int):
         img_name = self.img_names[idx]
         target = self.annotations[img_name]
         img = Image.open(self.path.joinpath('images', img_name)).convert('RGB')
@@ -189,7 +186,7 @@ class RecognitionDatasetCustom(Dataset):
 
 class DetectionDatasetCustom(RecognitionDatasetCustom):
 
-    def __getitem__(self, idx):
+    def __getitem__(self, idx: int):
         img_name = self.img_names[idx]
         boxes = self.annotations[img_name]['boxes']
         boxes = np.array(boxes)
@@ -231,11 +228,16 @@ class ColorShiftTorch(v2.Transform):
 
 
 class ColorShiftAlb(BasicTransform):
-    def __init__(self, min_val: float = .5, always_apply: bool | None = None, p: float = .5):
+    def __init__(
+        self, 
+        min_val: float = .5, 
+        always_apply: bool | None = None, 
+        p: float = .5
+    ):
         super().__init__(p=p, always_apply=always_apply)
         self.min_val = min_val
 
-    def apply(self, img, **params):
+    def apply(self, img: np.ndarray, **params) -> np.ndarray:
         return self._color_shift(img, self.min_val)
 
     def get_transform_init_args_names(self):
